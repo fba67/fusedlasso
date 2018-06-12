d6ce0b07a6927309c690171b8bcba3fd937e9c73
Fateme <fateme@eduroam3-0522.funklan.uni-saarland.de>
2018-06-12 15:54:51 +0200

    fixed d2 in simulation 7

--- simulation/generate_sim_data.R
+++ simulation/generate_sim_data.R
@@ -204,7 +204,7 @@ library(MASS)
 
 ### Generate indices for segments of size segment.size
 d1 <- matrix(mvrnorm(N * 40, mu= data.mean[seq(40)], Sigma= data.cov1), ncol= 40, nrow= N, byrow=T)
-d2 <- matrix(mvrnorm(N * 40, mu= data.mean[seq(41, 80)], Sigma= data.cov1), ncol= 40, nrow= N, byrow=T)
+d2 <- matrix(mvrnorm(N * 40, mu= data.mean[seq(41, 80)], Sigma= data.cov2), ncol= 40, nrow= N, byrow=T)
 d <- cbind(d1, d2)
 ### Assign ~0 to every other segment
 data.x <- d
